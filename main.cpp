e6f076fb1465dcb4780da387bea3f4984a2804bf
Edson Shivuri <eshivuri@sentillia.com>
2025-04-21 18:09:05 +0000

    chore(db): added default mongo setup

--- main.cpp
+++ main.cpp
@@ -7,10 +7,47 @@
 #include "httplib.h"
 using json = nlohmann::json;
 
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+
 #include "auth/auth.h"
 
 int main(void) {
   httplib::Server server;
+  mongocxx::instance inst{};
+
+  const auto uri = mongocxx::uri{
+      "mongodb+srv://"
+      "edsonshivuri:7UgntlDCyRsn7ob4@cluster0.ljbvmqh.mongodb.net/"
+      "?retryWrites=true&w=majority&appName=Cluster0"};
+
+  try {
+    // Create an instance.
+    mongocxx::instance inst{};
+    const auto uri = mongocxx::uri{
+        "mongodb+srv://"
+        "edsonshivuri:7UgntlDCyRsn7ob4@cluster0.ljbvmqh.mongodb.net/"
+        "?retryWrites=true&w=majority&appName=Cluster0"};
+    // Set the version of the Stable API on the client
+    mongocxx::options::client client_options;
+    const auto api = mongocxx::options::server_api{
+        mongocxx::options::server_api::version::k_version_1};
+    client_options.server_api_opts(api);
+    // Setup the connection and get a handle on the "admin" database.
+    mongocxx::client conn{uri, client_options};
+    mongocxx::database db = conn["admin"];
+    // Ping the database.
+    const auto ping_cmd = bsoncxx::builder::basic::make_document(
+        bsoncxx::builder::basic::kvp("ping", 1));
+    db.run_command(ping_cmd.view());
+    std::cout
+        << "Pinged your deployment. You successfully connected to MongoDB!"
+        << std::endl;
+  } catch (const std::exception &e) {
+    // Handle errors
+    std::cout << "Exception: " << e.what() << std::endl;
+  }
 
   server.Get("/", [](const httplib::Request &req, httplib::Response &res) {
     res.set_content("<h1>Hello !!</h1>", "text/html");
