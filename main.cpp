6090267fc095812e4328503256d00788e867e590
Edson Shivuri <eshivuri@sentillia.com>
2025-04-23 06:56:46 +0000

    chore(db): set up the db connection

--- main.cpp
+++ main.cpp
@@ -13,6 +13,9 @@ using json = nlohmann::json;
 
 #include "auth/auth.h"
 
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::make_document;
+
 int main(void) {
   httplib::Server server;
 
@@ -23,21 +26,19 @@ int main(void) {
         "mongodb+srv://"
         "edsonshivuri:7UgntlDCyRsn7ob4@cluster0.ljbvmqh.mongodb.net/"
         "?retryWrites=true&w=majority&appName=Cluster0"};
+
     // Set the version of the Stable API on the client
     mongocxx::options::client client_options;
     const auto api = mongocxx::options::server_api{
         mongocxx::options::server_api::version::k_version_1};
     client_options.server_api_opts(api);
+
     // Setup the connection and get a handle on the "admin" database.
     mongocxx::client conn{uri, client_options};
-    mongocxx::database db = conn["admin"];
-    // Ping the database.
-    const auto ping_cmd = bsoncxx::builder::basic::make_document(
-        bsoncxx::builder::basic::kvp("ping", 1));
-    db.run_command(ping_cmd.view());
-    std::cout
-        << "Pinged your deployment. You successfully connected to MongoDB!"
-        << std::endl;
+
+    auto collection = conn["testdb"]["testcollection"];
+
+    collection.insert_one(make_document(kvp("test", "passed")));
   } catch (const std::exception &e) {
     // Handle errors
     std::cout << "Exception: " << e.what() << std::endl;
